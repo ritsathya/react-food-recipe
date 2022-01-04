183371aff73dd1743db9257d834a1e0f804743e7
ritsathya <pisithritsathya@gmail.com>
2022-01-04 21:04:59 +0700

    add more images and create state in Home.jsx

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardItem from "./CardItem";
 
 const FoodCard = (props) => {
+  const array = props.array;
   return (
     <div className="cards">
       <h2>
@@ -12,46 +13,46 @@ const FoodCard = (props) => {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/bread.jpg"
-              text="Yummy Bread"
+              src={array[0].imgSrc}
+              text={array[0].recipeName}
               label={props.label}
               path="/services"
             />
             <CardItem
-              src="images/cereal.jpg"
-              text="Cereal"
+              src={array[1].imgSrc}
+              text={array[1].recipeName}
               label={props.label}
               path="/services"
             />
             <CardItem
-              src="images/cereal.jpg"
-              text="Cereal"
+              src={array[2].imgSrc}
+              text={array[2].recipeName}
               label={props.label}
               path="/services"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/egg-toast.jpg"
-              text="Egg Toast"
+              src={array[3].imgSrc}
+              text={array[3].recipeName}
               label={props.label}
               path="/services"
             />
             <CardItem
-              src="images/french-toast.jpg"
-              text="French Toast"
+              src={array[4].imgSrc}
+              text={array[4].recipeName}
               label={props.label}
               path="/services"
             />
             <CardItem
-              src="images/french-toast.jpg"
-              text="French Toast"
+              src={array[5].imgSrc}
+              text={array[5].recipeName}
               label={props.label}
               path="/services"
             />
             <CardItem
-              src="images/strawberry-pancakes.jpg"
-              text="Strawberry Pancakes"
+              src={array[6].imgSrc}
+              text={array[6].recipeName}
               label={props.label}
               path="/services"
             />
