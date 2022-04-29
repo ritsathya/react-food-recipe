8d575c81e0342a2be5450847fd262b5219d02655
Chhay-Lay <ngov.chhaylay.2019@rupp.edu.kh>
2022-04-29 15:51:34 +0700

    register-backend

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,8 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+
 import Navbar from "../Navbar";
 
 const Register = () => {
+  let navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email_or_phoneNumber, setEmail_or_phoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
+
+  const signUp = e =>
+  {
+    e.preventDefault();
+    if(password == password_confirmation)
+    {
+      let data = {};
+            
+      if(email_or_phoneNumber.includes('@'))
+      {
+        let email = email_or_phoneNumber;
+        data = {name, email, password, password_confirmation}
+        alert(email)
+      }else{
+        let phone_number = email_or_phoneNumber;
+        data = {name, phone_number, password, password_confirmation}
+      }
+      console.log(data)
+      axios.post("register", data)
+        .then(res => {
+          console.log(res);
+          
+          if(res.data.message.includes("Duplicate")){
+            alert("Email or Phone Number is already exist in our platform!")
+          }
+          localStorage.setItem("user-token", res.data.token);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        
+        // if(localStorage.getItem("user-token")) navigate(-2);
+    }else{
+      alert("Password not match!")
+    }
+  }
+
   return (
     <>
       <div>
@@ -11,7 +55,7 @@ const Register = () => {
         <div className="login flex-center">
           <div className="login__form">
             <h3 className="mt-4">Create an Account</h3>
-            <form className="m-4 mt-3">
+            <form onSubmit={signUp} className="m-4 mt-3">
               <div className="mb-3">
                 <label htmlFor="inputEmailPhone" className="form-label">
                   EMAIL OR PHONE NUMBER <font style={{ color: "red" }}>*</font>
@@ -20,6 +64,10 @@ const Register = () => {
                   type="text"
                   className="form-control"
                   id="inputEmailPhone"
+                  minlength="9"
+                  value={email_or_phoneNumber}
+                  onChange={(e) => setEmail_or_phoneNumber(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-2">
@@ -30,6 +78,9 @@ const Register = () => {
                   type="text"
                   className="form-control"
                   id="inputUsername"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-2">
@@ -40,6 +91,10 @@ const Register = () => {
                   type="password"
                   className="form-control"
                   id="inputPassword"
+                  value={password}
+                  minlength="8"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </div>
               <div className="mb-2">
@@ -50,6 +105,10 @@ const Register = () => {
                   type="password"
                   className="form-control"
                   id="inputConfirmPassword"
+                  minlength="8"
+                  value={password_confirmation}
+                  onChange={(e) => setPassword_confirmation(e.target.value)}
+                  required
                 />
               </div>
               <div className="btn-section">
